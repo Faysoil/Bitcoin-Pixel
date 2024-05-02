fb0e6b078e72ed7c568addafeb351f2a5de598ed
Kenza002 <113637652+Kenza002@users.noreply.github.com>
2024-05-02 15:29:39 +0200

    Update app.py
    block

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import http.server
 import socketserver
+import json
+import hashlib
+import string
+import random
 
 PORT = 9090
 
@@ -21,3 +25,25 @@ class MyHttpRequestHandler(http.server.SimpleHTTPRequestHandler):
 with socketserver.TCPServer(("", PORT), MyHttpRequestHandler) as httpd:
     print(f"Le serveur est démarré sur le port {PORT}")
     httpd.serve_forever()
+
+def randomNounce():
+    number_of_strings = 5
+    length_of_string = 8
+    for x in range(number_of_strings):
+        return (
+            "".join(
+                random.choice(string.ascii_letters + string.digits)
+                for _ in range(length_of_string)
+            )
+        )
+
+
+class Block:
+    def __init__(self, prev_id, data, height):
+        self.prev_id = prev_id
+        if not isinstance(data, list) or len(data) == 0:
+            raise ValueError("Data must be a non-empty list") 
+        self.data = data
+        self.height = height
+        self.nonce = randomNounce()
+        self.id = sha256()
